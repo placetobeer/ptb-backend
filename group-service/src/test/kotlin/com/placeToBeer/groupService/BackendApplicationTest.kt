fa00061c53005691c04c39d12d6ac0899fa20162
BuildTools <unconfigured@null.spigotmc.org>
2020-11-08 00:40:33 +0100

    first commit

---
+++ group-service/src/test/kotlin/com/placeToBeer/groupService/BackendApplicationTest.kt
@@ -0,0 +1,11 @@
+package com.placeToBeer.groupService
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
