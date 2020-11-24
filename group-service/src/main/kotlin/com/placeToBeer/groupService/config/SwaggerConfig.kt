59ab12574f429785f3e0a8556cdc3b8b0c729fd5
wiuwiu1 <pa.welter@web.de>
2020-11-24 09:59:16 +0100

    modified swagger configuration

--- group-service/src/main/kotlin/com/placeToBeer/groupService/config/SwaggerConfig.kt
+++ group-service/src/main/kotlin/com/placeToBeer/groupService/config/SwaggerConfig.kt
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport
+import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
@@ -18,8 +20,16 @@ open class SwaggerConfig: WebMvcConfigurationSupport() {
     open fun api(): Docket {
         return Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.placeToBeer.groupService.restPresenters"))
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData())
+    }
+
+    @Bean
+    open fun metaData(): ApiInfo {
+        return ApiInfoBuilder()
+                .title("REST API of PlaceToBeer backend application")
                 .build()
     }
 
