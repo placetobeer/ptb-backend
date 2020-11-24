38065c3bb238f49d613d544280a70a369f9ffc08
wiuwiu1 <pa.welter@web.de>
2020-11-24 12:39:29 +0100

    modified GroupController and added Swagger annotations

--- group-service/src/main/kotlin/com/placeToBeer/groupService/restPresenters/GroupController.kt
+++ group-service/src/main/kotlin/com/placeToBeer/groupService/restPresenters/GroupController.kt
@@ -2,16 +2,21 @@ package com.placeToBeer.groupService.restPresenters
 
 import com.placeToBeer.groupService.entities.Group
 import com.placeToBeer.groupService.services.GroupService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-
+@Api(description = "Operations directly related to groups")
+@RequestMapping("/groups")
 class GroupController (private val groupService: GroupService){
 
-    @GetMapping("/group")
-    fun getGroupListByUserId(@RequestParam UserId: Int): List<Group>{
-        return groupService.getGroupList(UserId)
+    @ApiOperation(value = "View list of groups that refer to the user by membership")
+    @RequestMapping(value = ["/byUserId/{userId}"], method = [RequestMethod.GET], produces = ["application/json"])
+    fun getGroupListByUserId(@PathVariable userId: Int): List<Group>{
+        return groupService.getGroupList(userId)
     }
 }
