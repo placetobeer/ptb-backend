bf235c701a6dc40f557e0dc6c2589cb75ce6b42b
wiuwiu1 <pa.welter@web.de>
2020-12-01 18:49:08 +0100

    refactored GroupController getGroupListByUserId: - changed resource address and params

--- group-service/src/main/kotlin/com/placeToBeer/groupService/restPresenters/GroupController.kt
+++ group-service/src/main/kotlin/com/placeToBeer/groupService/restPresenters/GroupController.kt
@@ -4,10 +4,7 @@ import com.placeToBeer.groupService.entities.Group
 import com.placeToBeer.groupService.services.GroupService
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @Api(description = "Operations directly related to groups")
@@ -15,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController
 class GroupController (private val groupService: GroupService){
 
     @ApiOperation(value = "View list of groups that refer to the user by membership")
-    @RequestMapping(value = ["/byUserId/{userId}"], method = [RequestMethod.GET], produces = ["application/json"])
-    fun getGroupListByUserId(@PathVariable userId: Int): List<Group>{
+    @GetMapping(value = [""], params = ["userId"],produces = ["application/json"])
+    fun getGroupListByUserId(@RequestParam userId: Int): List<Group>{
         return groupService.getGroupList(userId)
     }
 }
