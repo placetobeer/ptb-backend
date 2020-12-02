ddef0784bfe243b37973137e5e9926d32cba746b
bloup-dv <naujox.tom@gmail.com>
2020-12-03 00:35:20 +0100

    added @NotNull annotation to all attributes

--- group-service/src/main/kotlin/com/placeToBeer/groupService/entities/Membership.kt
+++ group-service/src/main/kotlin/com/placeToBeer/groupService/entities/Membership.kt
@@ -1,19 +1,24 @@
 package com.placeToBeer.groupService.entities
 
 
+import org.jetbrains.annotations.NotNull
 import javax.persistence.*
 
 
 @Entity
-//@Table() //schema = "memberships"
+@Table(name = "membership") //schema = "memberships"
 data class Membership(
         @Id
+        @NotNull
         @GeneratedValue(strategy = GenerationType.AUTO)
         val membershipID: Long,
+        @NotNull
         @ManyToOne()
         val group: Group,
+        @NotNull
         @ManyToOne()
         val member : User,
+        @NotNull
         var role : Role) {
         constructor(): this(0, Group(), User(), Role.MEMBER) {
         }
--- group-service/src/main/kotlin/com/placeToBeer/groupService/entities/User.kt
+++ group-service/src/main/kotlin/com/placeToBeer/groupService/entities/User.kt
@@ -1,14 +1,17 @@
 package com.placeToBeer.groupService.entities
 
+import org.jetbrains.annotations.NotNull
 import javax.persistence.*
 
 @Entity
-//@Table() //schema = "users"
+@Table(name = "user") //schema = "users"
 data class User
         constructor(
         @Id
+        @NotNull
         @GeneratedValue(strategy = GenerationType.AUTO)
         var id:Long,
+        @Column(nullable = false)
         var name:String){
         constructor():this(0, "")
 }
