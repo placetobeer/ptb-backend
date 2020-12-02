ac0530fd35718296ec92c947d8180c49de744b92
bloup-dv <naujox.tom@gmail.com>
2020-12-03 00:15:29 +0100

    added function "findByMember()" to MembershipRepository.kt

--- group-service/src/main/kotlin/com/placeToBeer/groupService/gateways/MembershipRepository.kt
+++ group-service/src/main/kotlin/com/placeToBeer/groupService/gateways/MembershipRepository.kt
@@ -1,6 +1,8 @@
 package com.placeToBeer.groupService.gateways
 import com.placeToBeer.groupService.entities.Membership
+import com.placeToBeer.groupService.entities.User
 import org.springframework.data.repository.CrudRepository
 
 interface MembershipRepository : CrudRepository<Membership, Long> {
+    fun findByMember(user:User):List<Membership>
 }
