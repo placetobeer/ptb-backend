ca9f021655fce4da7323871917721c690ccec1a8
wiuwiu1 <pa.welter@web.de>
2020-12-05 00:50:28 +0100

    added mock data

---
+++ group-service/src/main/resources/data.sql
@@ -0,0 +1,24 @@
+INSERT INTO friend_group (name) VALUES
+('Bratis Kartoffeln'),
+('Hüttengaudis');
+
+
+INSERT INTO user (name) VALUES
+('Bea'),
+('Jonas'),
+('Katja'),
+('Patrick'),
+('Lucie'),
+('Tom');
+
+INSERT INTO membership (group_id, member_id, role) VALUES
+('1', '1', '2'),
+('1', '2', '1'),
+('1', '3', '1'),
+('1', '4', '0'),
+('1', '5', '0'),
+('1', '6', '0'),
+('2', '4', '2'),
+('2', '5', '1'),
+('2', '6', '1');
+
