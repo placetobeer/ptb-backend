cbab9afaf1f90e786694e2fd0d86f4f85d7e4923
wiuwiu1 <pa.welter@web.de>
2021-04-11 12:49:00 +0200

    PTB: Refactor splits groupService logic into interactors creates validator plugins

---
+++ group-service/src/main/kotlin/com/placeToBeer/groupService/interactors/SetGroupNameInteractor.kt
@@ -0,0 +1,25 @@
+package com.placeToBeer.groupService.interactors
+
+import com.placeToBeer.groupService.entities.Group
+import com.placeToBeer.groupService.gateways.GroupRepository
+import com.placeToBeer.groupService.plugins.GroupExistValidatorPlugin
+import com.placeToBeer.groupService.plugins.GroupNameValidatorPlugin
+import org.springframework.stereotype.Component
+
+@Component
+class SetGroupNameInteractor(private var groupRepository: GroupRepository, private var groupNameValidatorPlugin: GroupNameValidatorPlugin,
+                             private var groupExistValidatorPlugin: GroupExistValidatorPlugin){
+
+    fun execute(groupId: Long, groupName: String) {
+        groupNameValidatorPlugin.validate(groupName)
+        val group = getGroupByGroupId(groupId)
+        group.name = groupName
+        groupRepository.save(group)
+    }
+
+    private fun getGroupByGroupId(groupId: Long): Group {
+        val possibleGroup = groupRepository.findById(groupId)
+        return groupExistValidatorPlugin.validateAndReturn(possibleGroup, groupId)
+    }
+
+}
---
+++ group-service/src/main/kotlin/com/placeToBeer/groupService/plugins/GroupNameValidatorPlugin.kt
@@ -0,0 +1,26 @@
+package com.placeToBeer.groupService.plugins
+
+import com.placeToBeer.groupService.exceptions.GroupNameIsInvalidException
+import com.placeToBeer.groupService.exceptions.GroupNotFoundException
+import com.placeToBeer.groupService.exceptions.UserNotFoundException
+import com.placeToBeer.groupService.services.GroupService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
+
+@Component
+class GroupNameValidatorPlugin {
+
+    private var logger: Logger = LoggerFactory.getLogger(GroupService::class.java)
+
+    fun validate(groupName: String) {
+        if(groupName.isNullOrEmpty()){
+            throwGroupNameIsInvalidError(groupName)
+        }
+    }
+
+    private fun throwGroupNameIsInvalidError(groupName: String) {
+        logger.error("Group name $groupName is invalid")
+        throw GroupNameIsInvalidException(groupName)
+    }
+}
