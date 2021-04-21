2dc6f702030c1e7fdd81922476007f16c3f03dd0
bloup-dv <naujox.tom@gmail.com>
2021-04-22 01:41:39 +0200

    PTB-106: deleted obsulete CreateMembershipInteractor.kt and implemented its functionality into AnswerInvitationInteractor.kt

--- group-service/src/main/kotlin/com/placeToBeer/groupService/interactors/invitation/AnswerInvitationInteractor.kt
+++ group-service/src/main/kotlin/com/placeToBeer/groupService/interactors/invitation/AnswerInvitationInteractor.kt
@@ -1,26 +1,59 @@
 package com.placeToBeer.groupService.interactors.invitation
 
-import com.placeToBeer.groupService.entities.Invitation
+import com.placeToBeer.groupService.entities.*
+import com.placeToBeer.groupService.gateways.GroupRepository
 import com.placeToBeer.groupService.gateways.InvitationRepository
-import com.placeToBeer.groupService.interactors.membership.CreateMembershipInteractor
+import com.placeToBeer.groupService.gateways.MembershipRepository
+import com.placeToBeer.groupService.gateways.UserRepository
+import com.placeToBeer.groupService.plugins.GroupExistValidatorPlugin
 import com.placeToBeer.groupService.plugins.InvitationExistsValidatorPlugin
+import com.placeToBeer.groupService.plugins.UserExistValidatorPlugin
 import org.springframework.stereotype.Component
 
 @Component
 class AnswerInvitationInteractor(
     private var invitationRepository: InvitationRepository,
+    private var membershipRepository: MembershipRepository,
+    private var userRepository: UserRepository,
+    private var groupRepository: GroupRepository,
     private var invitationExistsValidatorPlugin: InvitationExistsValidatorPlugin,
-    private var createMembershipInteractor: CreateMembershipInteractor
+    private var userExistValidatorPlugin: UserExistValidatorPlugin,
+    private var groupExistValidatorPlugin: GroupExistValidatorPlugin
 ) {
     fun execute(invitationId:Long, decision: Boolean){
         val invitation = getInvitationById(invitationId)
         if (decision){
             val groupId = invitation.group.id ?: 0
             val userId = invitation.recipient.id ?: 0
-            createMembershipInteractor.execute(groupId, userId, invitation.role)
+            createMembershipByIds(groupId, userId, invitation.role)
             //delete invitation before creating Membership?
         }
         deleteInvitation(invitation)
+    }
+
+    private fun createMembershipByIds( groupId: Long,userId: Long, role: Role){
+        val group = getGroupByGroupId(groupId)
+        val user = getUserByUserId(userId)
+        createMembership(group, user, role)
+    }
+
+    private fun createMembership(group: Group, user: User, role: Role){
+        val newMembership = Membership(
+            group,
+            user,
+            role
+        )
+        membershipRepository.save(newMembership)
+    }
+
+    private fun getUserByUserId(userId:Long): User {
+        val user = userRepository.findById(userId)
+        return userExistValidatorPlugin.validateAndReturn(user,userId)
+    }
+
+    private fun getGroupByGroupId(groupId: Long): Group {
+        val group = groupRepository.findById(groupId)
+        return groupExistValidatorPlugin.validateAndReturn(group,groupId)
     }
 
     private fun deleteInvitation(invitation: Invitation){
