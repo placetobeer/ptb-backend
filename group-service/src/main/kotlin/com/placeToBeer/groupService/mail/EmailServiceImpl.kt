c03efb34b4339e0c8d903f99cb060e968302f7ca
bloup-dv <naujox.tom@gmail.com>
2021-05-20 01:50:58 +0200

    PTB-96: added EmailServiceImpl.kt which let's us send a custom SimpleMessage

---
+++ group-service/src/main/kotlin/com/placeToBeer/groupService/mail/EmailServiceImpl.kt
@@ -0,0 +1,22 @@
+package com.placeToBeer.groupService.mail
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.stereotype.Service
+
+@Service("EmailService")
+class EmailServiceImpl: EmailService {
+
+    @Autowired
+    lateinit var emailSender: JavaMailSender
+
+    override fun sendSimpleMessage(to: String, subject: String, text: String) {
+        var message: SimpleMailMessage = SimpleMailMessage()
+        message.from = "placetobeer.noreply@gmail.com"
+        message.setTo(to)
+        message.subject = subject
+        message.text = text
+        emailSender.send(message)
+    }
+}
