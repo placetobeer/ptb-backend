103d353971fdd4e2d75e82c4284d99d90a90b55e
bloup-dv <naujox.tom@gmail.com>
2021-05-24 16:35:21 +0200

    PTB-137: refactored InvitationControllerIntegrationTest.kt - works now (added ids to mock objects)

--- group-service/src/test/kotlin/com/placeToBeer/groupService/restPresenters/InvitationControllerIntegrationTest.kt
+++ group-service/src/test/kotlin/com/placeToBeer/groupService/restPresenters/InvitationControllerIntegrationTest.kt
@@ -29,14 +29,15 @@ class InvitationControllerIntegrationTest(
         private val validUserId: Long = 2
         private val invalidUserId: Long = 1
         private val invitationList = listOf(
-            Invitation("bea@mail.com",User(1,"Bea", "bea@mail.com"),User(2,"Patrick","patrick@mail.com"), Group(1,"Club Crew"), Role.MEMBER)
+            Invitation(1L,"bea@mail.com",User(1,"Bea", "bea@mail.com"),User(2,"Patrick","patrick@mail.com"), Group(1,"Club Crew"), Role.MEMBER)
         )
         private val validInvitationResponseList = listOf(
-            InvitationResponse(Invitation("bea@mail.com",User(1,"Bea","bea@mail.com"),User(2,"Patrick","patrick@mail.com"), Group(1,"Club Crew"), Role.MEMBER))
+            InvitationResponse(Invitation(1L,"bea@mail.com",User(1,"Bea","bea@mail.com"),User(2,"Patrick","patrick@mail.com"), Group(1,"Club Crew"), Role.MEMBER))
         )
 
         @Test
         fun whenGetInvitationResponseListByUserId_withValidUserId_thenReturnHttp200(){
+            whenever(mockInvitationService.getInvitationsListByUserId(validUserId)).thenReturn(validInvitationResponseList)
             mockMvc.perform(MockMvcRequestBuilders.get("/invitations")
                 .param("userId","$validUserId"))
                 .andExpect(MockMvcResultMatchers.status().isOk)
