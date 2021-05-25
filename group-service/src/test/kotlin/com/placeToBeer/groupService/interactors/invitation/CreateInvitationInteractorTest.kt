2ef05e4590bf216f2ff22b19dda632c4ff631052
lucie <weber_lucie@web.de>
2021-05-25 22:11:39 +0200

    PTB-95: add interactor test bad cases

--- group-service/src/test/kotlin/com/placeToBeer/groupService/interactors/invitation/CreateInvitationInteractorTest.kt
+++ group-service/src/test/kotlin/com/placeToBeer/groupService/interactors/invitation/CreateInvitationInteractorTest.kt
@@ -7,6 +7,9 @@ import com.placeToBeer.groupService.entities.Role
 import com.placeToBeer.groupService.entities.User
 import com.placeToBeer.groupService.entities.requests.InvitationItem
 import com.placeToBeer.groupService.entities.requests.InvitationRequest
+import com.placeToBeer.groupService.exceptions.EmailIsInvalidException
+import com.placeToBeer.groupService.exceptions.UserNotFoundException
+import com.placeToBeer.groupService.exceptions.UserNotOwnerException
 import com.placeToBeer.groupService.gateways.GroupRepository
 import com.placeToBeer.groupService.gateways.InvitationRepository
 import com.placeToBeer.groupService.plugins.GroupExistValidatorPlugin
@@ -33,7 +36,7 @@ class CreateInvitationInteractorTest {
     private val validGroup = Group(1, "Bratis Kartoffeln")
     private val validInvitationRequest = InvitationRequest(1, User(4, "Patrick"), invitationList)
 
-    private val validInvitation = Invitation( "a.b@gmail.com", User(4, "Patrick"),  validGroup, Role.ADMIN)
+    private val validInvitation = Invitation( "a.b@gmail.com", User(4, "Patrick"), validGroup, Role.ADMIN)
     private val expectedInvitationList: List<Invitation> = listOf(validInvitation)
 
     private var exception: Exception? = null
@@ -44,19 +47,59 @@ class CreateInvitationInteractorTest {
     }
 
     init {
-        whenever(groupRepository.findById(validGroup.id)).thenReturn(Optional.of(validGroup))
+        whenever(groupRepository.findById(validGroup.id!!)).thenReturn(Optional.of(validGroup))
         whenever(invitationRepository.save(any())).thenAnswer { invocationOnMock -> invocationOnMock.arguments[0] }
         whenever(groupExistValidatorPlugin.validateAndReturn(Optional.of(validGroup), validGroup.id!!)).thenReturn(validGroup)
     }
 
     @Test
     fun whenExecuteWithValidInvitationRequest_ThenReturnSavedInvitations(){
-        whenever(invalidInvitationsValidatorPlugin.validate(validInvitation)).then { this.exception == null }
         val isSavedInvitations = doExecute(validInvitationRequest)
 
         Assertions.assertThat(isSavedInvitations).isEqualTo(expectedInvitationList)
         Assertions.assertThat(exception).isNull()
         expectedInvitationList.forEach {verify(invitationRepository, times(1)).save(it)}
+    }
+
+    @Test
+    fun whenExecuteWithNonExistingUserId_ThenThrowUserNotFoundException(){
+        val invalidUser = User(-1, "Patrick")
+        val invalidInvitationRequest = InvitationRequest(1, invalidUser, invitationList)
+        val invalidInvitation = Invitation( "a.b@gmail.com", invalidUser, validGroup, Role.ADMIN)
+        whenever(invalidInvitationsValidatorPlugin.validate(invalidInvitation)).thenThrow(UserNotFoundException(-1))
+
+        val isSavedInvitations = doExecute(invalidInvitationRequest)
+
+        Assertions.assertThat(isSavedInvitations).isNull()
+        Assertions.assertThat(this.exception).isExactlyInstanceOf(UserNotFoundException::class.java)
+    }
+
+    @Test
+    fun whenExecuteIfUserNotOwner_ThenThrowUserNotOwnerException(){
+        val invalidOwner = User(3, "Katja")
+        val invalidInvitationRequest = InvitationRequest(1, invalidOwner, invitationList)
+        val invalidInvitation = Invitation( "a.b@gmail.com", invalidOwner, validGroup, Role.ADMIN)
+        whenever(invalidInvitationsValidatorPlugin.validate(invalidInvitation)).thenThrow(UserNotOwnerException(3))
+
+        val isSavedInvitations = doExecute(invalidInvitationRequest)
+
+        Assertions.assertThat(isSavedInvitations).isNull()
+        Assertions.assertThat(this.exception).isExactlyInstanceOf(UserNotOwnerException::class.java)
+    }
+
+    @Test
+    fun whenExecuteWithNoValidEmailFormat_ThenThrowEmailIsInvalidException(){
+        val invalidInvitationList: MutableList<InvitationItem> = mutableListOf(
+            InvitationItem("iam@wrong", true)
+        )
+        val invalidInvitationRequest = InvitationRequest(1, User(4, "Patrick"), invalidInvitationList)
+        val invalidInvitation = Invitation( "iam@wrong", User(4, "Patrick"), validGroup, Role.ADMIN)
+        whenever(invalidInvitationsValidatorPlugin.validate(invalidInvitation)).thenThrow(EmailIsInvalidException("iam@wrong"))
+
+        val isSavedInvitations = doExecute(invalidInvitationRequest)
+
+        Assertions.assertThat(isSavedInvitations).isNull()
+        Assertions.assertThat(this.exception).isExactlyInstanceOf(EmailIsInvalidException::class.java)
     }
 
     private fun doExecute(invitationRequest: InvitationRequest): List<Invitation>? {
